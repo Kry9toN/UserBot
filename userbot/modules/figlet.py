333e10ef6acce905cc0c8ad4a7a4c69cc189de7e
MoveAngel <moveangel29@gmail.com>
2020-03-12 21:24:13 +0700

    LICENSE: Refactor version from 1.c to 1.d
    Signed-off-by: MoveAngel <moveangel29@gmail.com>

--- userbot/modules/figlet.py
+++ userbot/modules/figlet.py
@@ -3,7 +3,9 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
- 
+# Port to UserBot by @MoveAngel
+
+import asyncio
 import pyfiglet
 from userbot import CMD_HELP
 from userbot.events import register
--- userbot/modules/quotly.py
+++ userbot/modules/quotly.py
@@ -3,49 +3,48 @@
 # Licensed under the Raphielscape Public License, Version 1.d (the "License");
 # you may not use this file except in compliance with the License.
 #
-# Port to userbot by @MoveAngel
-
+# Port From UniBorg to UserBot by MoveAngel
 
 import datetime
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.events import register
 from userbot import bot, CMD_HELP
+from userbot.events import register
 
-@register(outgoing=True, pattern="^.sg(?: |$)(.*)")
+@register(outgoing=True, pattern="^.q(?: |$)(.*)")
 async def _(event):
     if event.fwd_from:
         return 
     if not event.reply_to_msg_id:
-       await event.edit("`Reply to any user message.`")
+       await event.edit("```Reply to any user message.```")
        return
     reply_message = await event.get_reply_message() 
     if not reply_message.text:
-       await event.edit("```reply to text message```")
+       await event.edit("```Reply to text message```")
        return
-    chat = "@SangMataInfo_bot"
+    chat = "@QuotLyBot"
     sender = reply_message.sender
     if reply_message.sender.bot:
-       await event.edit("`Reply to actual users message.`")
+       await event.edit("```Reply to actual users message.```")
        return
-    await event.edit("`Processing`")
+    await event.edit("```Making a Quote```")
     async with bot.conversation(chat) as conv:
           try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1031952739))
               await bot.forward_messages(chat, reply_message)
               response = await response 
           except YouBlockedUserError: 
-              await event.reply("`Please unblock @sangmatainfo_bot and try again`")
+              await event.reply("```Please unblock @QuotLyBot and try again```")
               return
-          if response.text.startswith("Forward"):
-             await event.edit("`can you kindly disable your forward privacy settings for good?`")
+          if response.text.startswith("Hi!"):
+             await event.edit("```Can you kindly disable your forward privacy settings for good?```")
           else: 
-             await event.edit(f"{response.message.message}")
-
+             await event.delete()   
+             await bot.forward_messages(event.chat_id, response.message)
 
 CMD_HELP.update({
-        "sangmata": 
-        ".sg \
-          \nUsage: View user history.\n"
+        "quotly": 
+        ".q \
+          \nUsage: Enhance ur text to sticker.\n"
     })
