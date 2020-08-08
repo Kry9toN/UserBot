58f5f81ac8fc5ed22d3511dca4c45109adb2a3e0
GudMeong <avada758@gmail.com>
2020-08-08 08:37:17 +0700

    Userbot: Publish Deezload module. big thanks fortifying for port
    Signed-off-by: GudMeong <avada758@gmail.com>

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -95,6 +95,9 @@ OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
 # Telegraph
 TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", None)
 
+# Deezloader
+DEEZER_ARL_TOKEN = os.environ.get("DEEZER_ARL_TOKEN", None)
+
 # Default .alive name
 ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
 
